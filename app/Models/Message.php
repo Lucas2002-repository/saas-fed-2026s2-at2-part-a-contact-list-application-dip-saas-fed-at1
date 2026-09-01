98a51f952b2bab5bb70d437a1e307f87c77a6898
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2026-09-01 10:40:35 +0800

    feat: add trait and casts and fillable

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Database\Factories\MessageFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Message extends Model
 {
-    //
+    /** @use HasFactory<MessageFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'topic_id',
+        'name',
+        'email',
+        'subject',
+        'message',
+        'read_at',
+    ];
+
+    protected function casts(): array
+// {
+//     return [
+//         'read_at' => 'datetime',
+//     ];
+// }
 }
