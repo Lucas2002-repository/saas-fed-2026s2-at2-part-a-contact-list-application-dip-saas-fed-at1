fbb182906b781d88d60915bab65cdcdf44119472
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2026-09-01 10:17:16 +0800

    feat: model creation

---
+++ app/Models/Topic.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+// use Database\Factories\TopicFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/** @use HasFactory<TopicFactory> */
+class Topic extends Model
+{
+    /** @use HasFactory<TopicFactory> */
+    use HasFactory;
+        //
+}
